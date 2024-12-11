0c3d17665b03419ab2439b9aeb4e398a6f427505
mericeltao <mst223@cornell.edu>
2024-12-10 23:41:34 -0500

    updated scraping, pushes to s3

--- get_files.py
+++ get_files.py
@@ -1,26 +1,37 @@
 import json
 import os
 import re
+import boto3
 
-os.makedirs('arxiv_papers')
+obj = boto3.client("s3")
+count = 0
+def send_to_s3(filename):
+    obj.upload_file(
+    Filename= filename,
+    Bucket="arxivpapers",
+    Key="arxiv_papers/"+filename
+)
 
 entries = []
 
 with open('test.json', 'r', encoding='utf-8') as file:
-    for i, line in enumerate(file):
-        if i >= 100:
-            break
-        entries.append(json.loads(line))
+    with open('test.json', 'r', encoding='utf-8') as file:
+        for i, line in enumerate(file):
+            if i < 31000:
+                entries.append(json.loads(line))
+            else: 
+                break
 
 for entry in entries:
     paper_id = entry.get("paper_id")
     paper_id = re.sub(r'\.', '_', paper_id)
     paper_id = paper_id.replace("/", "_").replace("\\", "_")
     text = entry.get("fulltext")
-    print(text)
     
-    # Check if required fields are present
     if paper_id and text:
-        txt_file_path = os.path.join('arxiv_papers', f"{paper_id}.txt")
-        with open(txt_file_path, 'w', encoding='utf-8') as txt_file:
+        with open(paper_id + '.txt', 'w', encoding='utf-8') as txt_file:
             txt_file.write(text)
+        send_to_s3(paper_id + '.txt')
+        count += 1
+        print(count)
+        os.remove(paper_id + '.txt')
